5c20afaba8eaee83b1124f08149c6fb01e5c5859
Piotr Kobylarczyk <46544957+moondatamancer@users.noreply.github.com>
2022-06-22 20:24:23 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,21 +2,12 @@
 
 mkdir DATA
 cd DATA
-# Place to store music andpodcasts
 mkdir Audio Audio/Music
-# E-book section
 mkdir Books
-# Downloads - can be remaped in system to a default directory
 mkdir Downloads
-
-#Images
 mkdir Images Images/Animated Images/Memes Images/Photos Images/Screenshots Images/Wallpapers
-
 mkdir Learning
 mkdir Projects
 mkdir Documents
-
 mkdir Software Software/Applications Software/Applications/Amdroid Software/Applications/Linux Software/Applications/Windows Software/Firmware
-
 mkdir Video Video/Movies Video/TV_Shows Video/Web
-
